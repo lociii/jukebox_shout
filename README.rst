c8a9ffea2e2a3e719d6bb7152f53665de8b7a101
lociii <opensource@jensnistler.de>
2013-05-18 12:13:36 +0200

    initial commit

---
+++ README.rst
@@ -0,0 +1,33 @@
+Jukebox shoutcast streaming playback module
+============================================
+
+jukebox-shout provides a daemon to stream your music to a shoutcast compatible server like `icecast <http://www.icecast.org>`_
+
+Basic setup instructions for icecast can be found in their `documentation <http://www.icecast.org/docs/icecast-2.3.2/icecast2_basicsetup.html>`_
+
+**Required system libraries**
+
+libshout3, libshout3-dev and python-dev are required to build the dependecy `python-shout <http://pypi.python.org/pypi/python-shout>`_.
+
+Startup
+========
+
+::
+
+    bin/jukebox jukebox_shout --start --host=[shoutcast_host] --port[shoutcast_port] --password=[shoutcast_source_password]
+
+Stop
+=====
+
+::
+
+    bin/jukebox jukebox_shout --stop
+
+Debugging
+========
+
+Add the --fg option to prevent the command from daemonizing.
+
+::
+
+    bin/jukebox jukebox_shout --fg --host=[shoutcast_host] --port[shoutcast_port] --password=[shoutcast_source_password]
---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="jukebox-shout",
+    packages=find_packages(),
+    version="0.1",
+    description="Shoutcast streaming playback service for jukebox",
+    author="Jens Nistler",
+    author_email="opensource@jensnistler.de",
+    url="http://jensnistler.de/",
+    download_url='http://github.com/lociii/jukebox_shout',
+    keywords=["jukebox", "music", "mp3", "playback", "backend", "shoutcast", "icecast"],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Sound/Audio :: Players",
+    ],
+    install_requires=[
+        "jukebox>=0.3.7",
+        "python-daemon==1.6",
+        "python-shout==0.2",
+    ],
+    include_package_data=True,
+    long_description=open("README.rst").read()
+)
